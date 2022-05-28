96895c49c8be075ab4154ca8c2c583c4dac0a87a
OMGOMGplays <loke@jenjen.se>
2022-05-28 12:02:00 +0200

    Why isn't code of work????

--- code/Entities/Purchasables/PurchasePlatform.cs
+++ code/Entities/Purchasables/PurchasePlatform.cs
@@ -1,6 +1,11 @@
 using Sandbox;
 using SandboxEditor;
 using System.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
 
 namespace SC
 {
@@ -8,7 +13,7 @@ namespace SC
 	[Library("purch_plat")]
 	[HammerEntity, EditorModel("models/Purchase/PurchaseBaseTemp.vmdl")]
 	[Model] // Just so it doesn't hide when shift+o'd in editor - Lokiv
-	public class PurchasePlatform : ModelEntity, IUse 
+	public class PurchasePlatform : ModelEntity
 	{
 		[Property, Title("Child Dropper"), Description("Changes which child dropper this is the parent to."), FGDType("target_destination")]
 		public Droppers ChildDropper {get; set;} // Work this thing out and like make it work and shit - Lokiv
@@ -16,34 +21,14 @@ namespace SC
 		[Property, Title("Price"), Description("Changes the price of this purchasable.")]
 		public int Price {get; set;} // Public for potential world text UI
 
-		public bool OnUse(Entity ent) 
-		{
-			var player = Local.Pawn as SCPlayer;
-			if (player.Money >= Price)
-			{
-				Purchase(ChildDropper);
-				return true;
-			}
-
-			Log.Info("Get more money bitch1!!");
-			return false;
-		}
-
-		public bool IsUsable(Entity ent) 
-		{
-			var player = Local.Pawn as SCPlayer;
-
-			if (player.Money < Price)
-				return false;
-
-			return true;
-		}
-
 		public override void Spawn()
 		{
 			base.Spawn();
 
 			EnableTouch = true;
+			UsePhysicsCollision = true;
+			MoveType = MoveType.None;
+			CollisionGroup = CollisionGroup.Always;
 
 			SetModel("models/purchase/PurchaseBaseTemp.vmdl");
 		}
@@ -51,6 +36,25 @@ namespace SC
 		public override void Simulate( Client cl )
 		{
 			base.Simulate( cl );
+
+			var tr = Trace.Ray(Position, Position += Vector3.Up * 15)
+				.UseHitboxes()
+				.Ignore(this)
+				.Size(15.0f)
+				.Run();
+
+			DebugOverlay.Sphere(Position, 15.0f, Color.Red, 0, true);
+
+			if (tr.Entity is SCPlayer player) 
+			{
+				if (player.Money >= Price)
+					Purchase(ChildDropper);
+				
+				else 
+				{
+					Log.Info("Get more money, now!!!");
+				}
+			}
 
 			if (ChildDropper.IsPurchased == true) 
 			{
@@ -67,7 +71,10 @@ namespace SC
 			{
 				if (player.Money >= Price)
 					Purchase(ChildDropper);
-				else Log.Info("Get more money bitch1!!");
+				else
+				{
+					Log.Info("Get more money bitch1!!");
+				}
 			}
 		}
 
